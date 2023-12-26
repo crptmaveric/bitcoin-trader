91ea8650243ac2f240f1d8ca68a79cf361aa53f2
rdy <rdvorsky@gmail.com>
2023-12-26 14:46:25 +0100

    Automaticke vytvaranie adresarov

--- install_libraries.sh
+++ install_libraries.sh
@@ -12,4 +12,21 @@ sudo apt-get install python3 python3-pip -y
 # Install necessary Python libraries
 pip3 install cryptography requests jwt numpy pandas schedule
 
+# Definícia cesty k priečinku s aplikáciou (upravte podľa vašich potrieb)
+APP_DIR="."
+LOGS_DIR="$APP_DIR/logs"
+CONFIG_DIR="$APP_DIR/config"
+
+# Kontrola existencie adresára logs
+if [ ! -d "$LOGS_DIR" ]; then
+  echo "Vytváram adresár 'logs'..."
+  mkdir -p "$LOGS_DIR"
+fi
+
+# Kontrola existencie adresára config
+if [ ! -d "$CONFIG_DIR" ]; then
+  echo "Vytváram adresár 'config'..."
+  mkdir -p "$CONFIG_DIR"
+fi
+
 echo "All necessary libraries have been installed."
